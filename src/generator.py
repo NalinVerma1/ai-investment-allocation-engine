910cd79df613b69a71e82ce3612e15a627732aa0
NalinVerma1 <NALIN.VERMA@UWATERLOO.CA>
2025-12-22 06:22:55 +0000

    generator

--- src/generator.py
+++ src/generator.py
@@ -1,1 +1,17 @@
+import intertools
 
+def generate_weights(n, step):
+    value = []
+    i = 0 
+    while i <= 1 :
+        values.append(round(i, 2))
+        i += step
+    
+    all_combi = intertools.product(values, repeat=n)
+
+    valid_weights = []
+    for i in all_combi:
+        if abs(sum(i) _1 ) < 0.0001:
+            valid_weights.append(i)
+    
+    return valid_weights
