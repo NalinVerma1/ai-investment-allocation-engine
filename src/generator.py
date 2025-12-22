24bf186351ba654bccda611bc7d1542d02406fe3
NalinVerma1 <NALIN.VERMA@UWATERLOO.CA>
2025-12-22 06:24:38 +0000

    fixes

--- src/generator.py
+++ src/generator.py
@@ -1,17 +1,17 @@
-import intertools
+import itertools
 
 def generate_weights(n, step):
     value = []
     i = 0 
     while i <= 1 :
-        values.append(round(i, 2))
+        value.append(round(i, 2))
         i += step
     
-    all_combi = intertools.product(values, repeat=n)
+    all_combi = itertools.product(values, repeat=n)
 
     valid_weights = []
     for i in all_combi:
-        if abs(sum(i) _1 ) < 0.0001:
+        if abs(sum(i) -1 ) < 0.0001:
             valid_weights.append(i)
     
     return valid_weights
