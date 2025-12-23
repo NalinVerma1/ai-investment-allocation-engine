b5fa798b5bb260d3aeb2ba296f8c20f315ee6fd1
NalinVerma1 <NALIN.VERMA@UWATERLOO.CA>
2025-12-23 02:43:42 +0000

    Connect Google Sheets and read assets table

---
+++ src/sheets_io.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+def open_sheet():
+    creds = Credentials.from_service_account_file(
+        os.getenv("GOOGLE_CREDENTIALS_FILE"),
+        scopes=SCOPES
+    )
+    client = gspread.authorize(creds)
+    return client.open_by_key(os.getenv("GOOGLE_SHEET_ID"))
+
+def read_df(tab_name):
+    sheet = open_sheet().worksheet(tab_name)
+    data = sheet.get_all_records()
+    return pd.DataFrame(data)
---
+++ src/test_sheets.py
@@ -0,0 +1,3 @@
+from sheets_io import read_df
+
+print(read_df("assets"))
